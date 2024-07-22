00a2428731d7e17fc6f1df16254f147a1e73cb8f
K Rones <kieran.rones@gmail.com>
2024-07-22 18:09:27 +0100

    Create configuration.py

---
+++ configuration.py
@@ -0,0 +1,17 @@
+"""
+Piper TTS functionality for audiobook creation.
+
+Author: Kieran Currie Rones
+Date: July 22nd, 2024
+
+This module contains the generation route for TTS.
+"""
+
+# Path to your voices folder
+VOICES_FOLDER = "./static/voices/"
+
+# Path to your output folder
+OUTPUT_FOLDER = "./static/output/"
+
+# Path to your piper executable
+PIPER_LOCATION = "./piper/piper"
